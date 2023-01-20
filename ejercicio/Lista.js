31e2f46385b694e112ac3209e26c32ef26103f5d
Nahuel Sosa <109542769+nahuelsosa122@users.noreply.github.com>
2023-01-20 14:27:16 -0300

    Add files via upload

---
+++ ejercicio/Lista.js
@@ -0,0 +1,12 @@
+const Lista = [
+    "Nahuel",
+    25,
+    true,
+    new Date= new Date(29,06,1997),
+    {
+        Titulo:"Frankenstein",
+        Autor: "Mary Wollstonecraft Shelley",
+        fecha: new Date(1,1,1818), 
+        URL:https:"//www.amazon.com/-/es/Mary-Shelley/dp/8491050892"
+    },
+];
